d9b009c08e277163b0f1389d69b51ee9e7f38fec
DGarbanzo <dirk.prange@web.de>
2019-11-18 22:27:20 +0000

    Improved Odometer: * change weight to 5% to even more smooth avg speed

--- donkeycar/templates/donkey3.py
+++ donkeycar/templates/donkey3.py
@@ -69,7 +69,7 @@ def drive(cfg, use_pid=False, no_cam=False, model_path=None, verbose=False):
 
     odo = Odometer(gpio=cfg.ODOMETER_GPIO,
                    tick_per_meter=cfg.TICK_PER_M,
-                   weight=0.1,
+                   weight=0.05,
                    debug=verbose)
     car.add(odo, outputs=['car/speed', 'car/inst_speed'])
     lap = LapTimer(gpio=cfg.LAP_TIMER_GPIO, trigger=4)

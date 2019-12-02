ca3a8365a0310170f228bf5f5fc10aa6d11815b9
DGarbanzo <dirk.prange@web.de>
2019-12-02 20:03:05 +0000

    Add webserver that only shows the camera as an alternative to donkey monitor * cleaning of imports * adding new webserver, serving only the camera frame * still overlapping code with the JPEG handler which will be cleaned after getting it to work * added base_fpv.html file for the simple FPV view

--- donkeycar/parts/web_controller/web.py
+++ donkeycar/parts/web_controller/web.py
@@ -5,8 +5,6 @@ Created on Sat Jun 24 20:10:44 2017
 
 @author: wroscoe
 
-remotes.py
-
 The client and web server needed to control a car remotely.
 """
 
@@ -17,8 +15,10 @@ import time
 import asyncio
 
 import requests
-import tornado.ioloop
-import tornado.web
+from tornado.ioloop import IOLoop
+from tornado.web import Application, RedirectHandler, StaticFileHandler, \
+    RequestHandler
+from tornado.httpserver import HTTPServer
 import tornado.gen
 from socket import gethostname
 
@@ -94,7 +94,7 @@ class RemoteWebServer():
         pass
 
 
-class LocalWebController(tornado.web.Application):
+class LocalWebController(Application):
 
     def __init__(self):
         '''
@@ -113,23 +113,23 @@ class LocalWebController(tornado.web.Application):
         self.port = 8887
 
         handlers = [
-            (r"/", tornado.web.RedirectHandler, dict(url="/drive")),
+            (r"/", RedirectHandler, dict(url="/drive")),
             (r"/drive", DriveAPI),
             (r"/video", VideoAPI),
-            (r"/static/(.*)", tornado.web.StaticFileHandler,
-              {"path": self.static_file_path}),
+            (r"/static/(.*)", StaticFileHandler,
+             {"path": self.static_file_path}),
         ]
 
         settings = {'debug': True}
         super().__init__(handlers, **settings)
-        print("... you can now go to {}local:8887 to drive "
+        print("... you can now go to {}.local:8887 to drive "
               "your car.".format(gethostname()))
 
     def update(self):
         ''' Start the tornado webserver. '''
         asyncio.set_event_loop(asyncio.new_event_loop())
         self.listen(self.port)
-        tornado.ioloop.IOLoop.instance().start()
+        IOLoop.instance().start()
 
     def run_threaded(self, img_arr=None):
         self.img_arr = img_arr
@@ -143,7 +143,7 @@ class LocalWebController(tornado.web.Application):
         pass
 
 
-class DriveAPI(tornado.web.RequestHandler):
+class DriveAPI(RequestHandler):
 
     def get(self):
         data = {}
@@ -161,10 +161,11 @@ class DriveAPI(tornado.web.RequestHandler):
         self.application.recording = data['recording']
 
 
-class VideoAPI(tornado.web.RequestHandler):
+class VideoAPI(RequestHandler):
     '''
     Serves a MJPEG of the images posted from the vehicle.
     '''
+
     async def get(self):
 
         self.set_header("Content-type",
@@ -188,5 +189,78 @@ class VideoAPI(tornado.web.RequestHandler):
                     await self.flush()
                 except tornado.iostream.StreamClosedError:
                     pass
+            else:
+                await tornado.gen.sleep(interval)
+
+
+class WebFpv(Application):
+    """
+    Class for running an FPV web server that only shows the camera in real-time.
+    The web page contains the camera view and auto-adjusts to the web browser
+    window size. Conjecture: this picture up-scaling is performed by the
+    client OS using graphics acceleration. Hence a web browser on the PC is
+    faster than a pure python application based on open cv or similar.
+    """
+
+    def __init__(self, port=8890):
+        self.port = port
+        #self.img_arr = None
+
+        """Construct and serve the tornado application."""
+        handlers = Application([
+            (r"/", BaseHandler),
+            (r"/video", MJPEGHandler),
+        ])
+
+        settings = {'debug': True}
+        super().__init__(handlers, **settings)
+        print("Started Web FPV server. You can now go to {}.local:{} to "
+              "view the car camera".format(gethostname(), self.port))
+
+    def update(self):
+        ''' Start the tornado webserver. '''
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        self.listen(self.port)
+        IOLoop.instance().start()
+
+    def run_threaded(self, img_arr=None):
+        self.img_arr = img_arr
+
+    def run(self, img_arr=None):
+        self.img_arr = img_arr
+
+    def shutdown(self):
+        pass
+
+
+class BaseHandler(RequestHandler):
+
+    def get(self):
+        data = {}
+        self.render("templates/base_fpv.html", **data)
+
+
+class MJPEGHandler(RequestHandler):
+
+    async def get(self):
+        self.set_header('Content-Type',
+                        'multipart/x-mixed-replace;boundary=--boundarydonotcross')
+        served_image_timestamp = time.time()
+        my_boundary = "--boundarydonotcross\n"
+        while True:
+
+            interval = .01
+            if served_image_timestamp + interval < time.time():
+                img = utils.arr_to_binary(self.application.img_arr)
+                self.write(my_boundary)
+                self.write("Content-type: image/jpeg\r\n")
+                self.write("Content-length: %s\r\n\r\n" % len(img))
+                self.write(img)
+                served_image_timestamp = time.time()
+                try:
+                    await self.flush()
+                except tornado.iostream.StreamClosedError:
+                    pass
+
             else:
                 await tornado.gen.sleep(interval)

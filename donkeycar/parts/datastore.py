689c3d9057280cf749722464d047d9948248b966
DGarbanzo <dirk.prange@web.de>
2019-12-15 15:39:24 +0000

    Store image array only as uint8 in order to save mem space - add queuesize log output

--- donkeycar/parts/datastore.py
+++ donkeycar/parts/datastore.py
@@ -501,6 +501,8 @@ class TubWriter(Tub):
         record[MS] = millis
         self.queue.put(record)
         self.queue_size = max(self.queue_size, self.queue.qsize())
+        if self.queue_size % 500 == 0 and self.queue_size > 0:
+            print('TubWriter queue size: {}'. format(self.queue_size))
         return self.current_ix
 
     def shutdown(self):

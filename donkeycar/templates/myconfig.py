10c4faacc1a4181a9d2166129b2bcbc1b0521f8f
DGarbanzo <dirk.prange@web.de>
2020-01-26 15:14:36 +0000

    Remove cropping

--- donkeycar/templates/myconfig.py
+++ donkeycar/templates/myconfig.py
@@ -60,7 +60,7 @@ FREEZE_LAYERS = False
 NUM_LAST_LAYERS_TO_TRAIN = 5  # flat, dense, dense, angle, throttle
 PACK_PATH = r'/Users/dirk/GoogleDrive/mycar/data'
 #EXCLUDE_SLOW_LAPS = 0.5
-ROI_CROP_TOP = 36
+# ROI_CROP_TOP = 36
 
 # AUTOPILOT
 AI_THROTTLE_MULT = 1.0

91caa904c6ddf36fed663e08e1a6367c4714d758
DGarbanzo <dirk.prange@web.de>
2020-02-28 20:19:38 +0000

    Set adafruit imu to use filter smoothing.

--- donkeycar/parts/imu.py
+++ donkeycar/parts/imu.py
@@ -63,6 +63,8 @@ class Mpu6050Ada:
         self.mpu = adafruit_mpu6050.MPU6050(i2c)
         self.mpu.accelerometer_range = adafruit_mpu6050.Range.RANGE_2_G
         self.mpu.gyro_range = adafruit_mpu6050.GyroRange.RANGE_250_DPS
+        # set filter to 44Hz data smoothing
+        self.mpu.filter_bandwidth(3)
         self.accel_zero = list(self.mpu.acceleration)
         self.gyro_zero = list(self.mpu.gyro)
         self.accel = list(self.accel_zero)

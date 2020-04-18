32870063fb8298eaaf34293ea47849013f5cb954
DGarbanzo <dirk.prange@web.de>
2020-04-18 11:27:18 +0100

    Some formatting, add missing myconfig argument that got lost in merge

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -3,15 +3,29 @@
 Scripts to drive a donkey 2 car
 
 Usage:
-    manage.py (drive) [--model=<model>] [--js] [--type=(linear|categorical|rnn|imu|behavior|3d|localizer|latent)] [--camera=(single|stereo)] [--meta=<key:value> ...]
-    manage.py (train) [--tub=<tub1,tub2,..tubn>] [--file=<file> ...] (--model=<model>) [--transfer=<model>] [--type=(linear|categorical|rnn|imu|behavior|3d|localizer)] [--continuous] [--aug]
-
+    manage.py (drive) [--model=<model>]
+                      [--js]
+                      [--type=(linear|categorical|rnn|imu|behavior|3d|localizer|latent)]
+                      [--camera=(single|stereo)]
+                      [--meta=<key:value> ...]
+                      [--myconfig=<filename>]
+    manage.py (train) [--tub=<tub1,tub2,..tubn>]
+                      [--file=<file> ...]
+                      (--model=<model>)
+                      [--transfer=<model>]
+                      [--type=(linear|categorical|rnn|imu|behavior|3d|localizer)]
+                      [--continuous]
+                      [--aug]
+                      [--myconfig=<filename>]
 
 Options:
     -h --help               Show this screen.
     --js                    Use physical joystick.
-    -f --file=<file>        A text file containing paths to tub files, one per line. Option may be used more than once.
-    --meta=<key:value>      Key/Value strings describing describing a piece of meta data about this drive. Option may be used more than once.
+    -f --file=<file>        A text file containing paths to tub files, one per
+                            line. Option may be used more than once.
+    --meta=<key:value>      Key/Value strings describing describing a piece of
+                            meta data about this drive. Option may be used more
+                            than once.
     --myconfig=filename     Specify myconfig file to use.
                             [default: myconfig.py]
 """

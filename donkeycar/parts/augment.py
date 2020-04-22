Tub augmentation: * fix augmented tub naming * factor out augmentation from scaling so it can be used on images directly * adding test for augmentation, small bugfix in path print, pep-8 reformatting

donkeycar/parts/augment.py
@@ -42,9 +42,38 @@ def rand_persp_transform(img):
 
 
 def augment_image(np_img, shadow_images=None, do_warp_persp=False):
+    """
+    :param np_img: numpy image
+        input image in numpy normalised format
+    :param shadow_images: list of 2-tuples of PIL images
+        shadow vector as prepared by load_shadow_images
+    :param do_warp_persp: bool
+        apply warping
+    :return: numpy image
+        output image in numpy normalised format
+    """
+    # denormalise image to 8int
     conv_img = np_img * 255.0
     conv_img = conv_img.astype(np.uint8)
+    # convert to PIL and apply transformation
     img = Image.fromarray(conv_img)
+    img = augment_pil_image(img, shadow_images, do_warp_persp)
+    # transform back to normalised numpy format
+    img_out = np.array(img).astype(np.float) * ONE_BY_255
+    return img_out
+
+
+def augment_pil_image(img, shadow_images=None, do_warp_persp=False):
+    """
+    :param img: PIL image
+        input image in PIL format
+    :param do_warp_persp: bool
+        apply warping
+    :param shadow_images: list of 2-tuples of PIL images
+        shadow vector as prepared by load_shadow_images
+    :return: PIL image
+        augmented image
+    """
     # change the coloration, sharpness, and composite a shadow
     factor = random.uniform(0.5, 2.0)
     img = ImageEnhance.Brightness(img).enhance(factor)
@@ -54,8 +83,7 @@ def augment_image(np_img, shadow_images=None, do_warp_persp=False):
     img = ImageEnhance.Sharpness(img).enhance(factor)
     factor = random.uniform(0.0, 2.0)
     img = ImageEnhance.Color(img).enhance(factor)
-
-    # optionally composite a shadow, perpared from load_shadow_images
+    # optionally composite a shadow, prepared from load_shadow_images
     if shadow_images is not None:
         iShad = random.randrange(0, len(shadow_images))
         top, mask = shadow_images[iShad]
@@ -65,12 +93,10 @@ def augment_image(np_img, shadow_images=None, do_warp_persp=False):
         mask = ImageEnhance.Brightness(mask).enhance(random.uniform(0.3, 1.0))
         offset = (random.randrange(-128, 128), random.randrange(-128, 128))
         img.paste(top, offset, mask)
-
     # optionally warp perspective
     if do_warp_persp:
         img = rand_persp_transform(img)
-
-    return np.array(img).astype(np.float) * ONE_BY_255
+    return img
 
 
 def load_shadow_images(path_mask):
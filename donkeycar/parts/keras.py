Multiple fixes: 1) In car app (donkey3) - move image normalisation only auto-pilot section, so no change of original data in recording 2) In KerasSquarePlusImu - fix calling of input when using non-tflite model and allow running w/o IMU but passing None input and using 0 in the model 3) Change batchsize 64 -> 1024, switch off tensorboard and add APPLY_GYRO flag to my_config 4) Fix random picture generation in profile, so far only black images were produced 5) Use APPLY_GYRO flag in training if EXCLUDE_SLOW_LAPS is set

donkeycar/parts/keras.py
@@ -203,10 +203,13 @@ class KerasSquarePlusImu(KerasSquarePlus):
         print('Created', self.__class__.__name__, 'imu_dim:', imu_dim,
               'NN size:', size)
 
-    def run(self, img_arr, imu):
+    def run(self, img_arr, imu=None):
         img_arr = img_arr.reshape((1,) + img_arr.shape)
-        imu_arr = np.array(imu)
-        outputs = self.model.predict(img_arr, imu_arr)
+        if imu is None:
+            imu_arr = np.zeros((1, 6))
+        else:
+            imu_arr = np.array(imu)
+        outputs = self.model.predict(x=[img_arr, imu_arr])
         steering = outputs[0]
         throttle = outputs[1]
         return steering[0][0], throttle[0][0]
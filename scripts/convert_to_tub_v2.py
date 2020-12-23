fae166bf1c0d648f67da85400c1456954e952242
DocGarbanzo <dirk.prange@web.de>
2020-12-23 09:26:55 +0000

    Incorporate PR feedback * Add empty record type into conversion script and update comments and variable names * minor fix in tub

--- scripts/convert_to_tub_v2.py
+++ scripts/convert_to_tub_v2.py
@@ -22,6 +22,8 @@ from donkeycar.parts.tub_v2 import Tub
 
 
 def convert_to_tub_v2(paths, output_path):
+    empty_record = {'__empty__': True}
+
     if type(paths) is str:
         paths = [paths]
     legacy_tubs = [LegacyTub(path) for path in paths]
@@ -30,33 +32,40 @@ def convert_to_tub_v2(paths, output_path):
 
     for legacy_tub in legacy_tubs:
         if not output_tub:
-            output_tub = Tub(output_path, legacy_tub.inputs,
-                             legacy_tub.types, list(legacy_tub.meta.items()))
+            # add input and type for empty records recording
+            inputs = legacy_tub.inputs + ['__empty__']
+            types = legacy_tub.types + ['boolean']
+            output_tub = Tub(output_path, inputs, types,
+                             list(legacy_tub.meta.items()))
 
         record_paths = legacy_tub.gather_records()
         bar = IncrementalBar('Converting', max=len(record_paths))
-        prev_rec_num = None
+        previous_index = None
         for record_path in record_paths:
             try:
                 contents = Path(record_path).read_text()
                 record = json.loads(contents)
                 image_path = record['cam/image_array']
-                rec_num = int(image_path.split('_')[0])
+                current_index = int(image_path.split('_')[0])
                 image_path = os.path.join(legacy_tub.path, image_path)
                 image_data = Image.open(image_path)
                 record['cam/image_array'] = image_data
                 # first record or they are continuous, just append
-                if not prev_rec_num or rec_num == prev_rec_num + 1:
+                if not previous_index or current_index == previous_index + 1:
                     output_tub.write_record(record)
-                    prev_rec_num = rec_num
+                    previous_index = current_index
                 # otherwise fill the gap with dummy records
                 else:
-                    prev_rec_num += 1
-                    while prev_rec_num < rec_num:
+                    # Skipping over previous record here because it has
+                    # already been written.
+                    previous_index += 1
+                    # Adding empty record nodes, and marking them deleted
+                    # until the next valid record.
+                    while previous_index < current_index:
                         idx = output_tub.manifest.current_index
-                        output_tub.write_record({})
+                        output_tub.write_record(empty_record)
                         output_tub.delete_record(idx)
-                        prev_rec_num += 1
+                        previous_index += 1
                 bar.next()
             except Exception as exception:
                 print(f'Ignoring record path {record_path}\n', exception)

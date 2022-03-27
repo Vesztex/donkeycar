7334ce38eb3457970248e77177f20072064a5081
DocGarbanzo <dirk.prange@web.de>
2022-03-27 12:59:50 +0100

    Add testing code

--- donkeycar/builder.py
+++ donkeycar/builder.py
@@ -1,3 +1,5 @@
+from copy import copy
+
 import yaml
 import graphviz
 import logging
@@ -37,6 +39,7 @@ class Builder:
                     arguments[k] = getattr(self.cfg, v[4:])
 
     def build_vehicle(self):
+        """ This function creates the car from the yaml file"""
         with open(self.car_file) as f:
             try:
                 car_description = yaml.safe_load(f)
@@ -68,14 +71,17 @@ class Builder:
         return car
 
     def plot_vehicle(self, car):
-
+        """ Function to provide a graphviz (dot) plot of the vehicle
+            architecture. This is meant to be an auxiliary helper to control
+            the data flow of input and output variables is as expected and
+            input, output and run_condition variables are correctly labelled
+            as they are free-form text strings. """
         g = graphviz.Digraph('Vehicle', filename='vehicle.py', format='png')
-
         g.attr('node', shape='box')
-        #g.attr('edge', splines='ortho')
 
         # build all parts as nodes first
-        for part_dict in car.parts:
+        car_parts = car.parts
+        for part_dict in car_parts:
             part = part_dict['part']
             name = part.__class__.__name__
             label = name
@@ -83,25 +89,77 @@ class Builder:
                 label += f'\n{k}: {str(v)}'
             g.node(name, label=label)
 
-        # connect all part with outputs to inputs
-        for part_dict in car.parts:
-            outputs = part_dict.get('outputs')
-            output_part_name = part_dict['part'].__class__.__name__
-            for output in outputs:
-                found = False
-                for part_dict_in in car.parts:
-                    inputs = part_dict_in['inputs']
-                    input_part_name = part_dict_in['part'].__class__.__name__
-                    if output in inputs:
-                        g.edge(output_part_name, input_part_name, label=output)
-                        found = True
-                    run_condition = part_dict_in.get('run_condition')
-                    # if output is a run condition draw different edge
-                    if run_condition == output:
-                        g.edge(output_part_name, input_part_name,
-                               label=output, style='dashed')
-                        found = True
-                if not found:
-                    g.edge(output_part_name, 'Unused', label=output)
+        # add all input, output, run_condition variables of all parts
+        variable_tracker = set()
+        for part_dict in car_parts:
+            name = part_dict['part'].__class__.__name__
+            variable_tracker.update((name, 'i', i) for i in part_dict.get('inputs', []))
+            variable_tracker.update((name, 'o', o) for o in part_dict.get('outputs', []))
+            run_condition = part_dict.get('run_condition')
+            if run_condition:
+                variable_tracker.add((name, 'r', run_condition))
+
+        reversed_parts = list(reversed(car_parts))
+        # connect all parts' outputs to inputs from top to bottom
+        self.traverse_parts(car_parts, g, variable_tracker, 'blue')
+        # reverse order and connect from bottom to top
+        self.traverse_parts(reversed_parts, g, variable_tracker, 'green')
+        # connected nodes now have all been removed, add the unconnected
+        for node in variable_tracker:
+            name, io, var = node
+            # no input defined for input or run_condition
+            if io in 'ir':
+                g.edge('Non defined', name, var, color='red')
+            # output goes nowhere
+            if io == 'o':
+                g.edge(name, 'Not used', var, color='red')
+            print(node)
         g.view()
+
+    @staticmethod
+    def traverse_parts(car_parts, g, nodes, color='blue'):
+        car_parts_copy = copy(car_parts)
+        for part_dict in car_parts:
+            outputs_upper = part_dict.get('outputs', [])
+            upper_part_name = part_dict['part'].__class__.__name__
+            print(f'Checking upper part {upper_part_name}')
+            # copy contains all parts that come after the current part
+            car_parts_copy.pop(0)
+            for output_upper in outputs_upper:
+                # here we cycle through all later parts
+                for part_dict_lower in car_parts_copy:
+                    inputs_lower = part_dict_lower.get('inputs', [])
+                    lower_part_name = part_dict_lower['part'].__class__.__name__
+                    print(f'Checking lower part {lower_part_name}')
+                    if output_upper in inputs_lower:
+                        g.edge(upper_part_name, lower_part_name,
+                               label=output_upper, color=color)
+                        found_output_upper = True
+                        # remove found entries from all nodes
+                        nodes.discard((upper_part_name, 'o', output_upper))
+                        nodes.discard((lower_part_name, 'i', output_upper))
+
+                    run_condition_lower = part_dict_lower.get('run_condition')
+                    # if output is a run condition draw different edge
+                    if run_condition_lower == output_upper:
+                        g.edge(upper_part_name, lower_part_name,
+                               label=output_upper, style='dotted',
+                               color=color)
+                        found_output_upper = True
+                        nodes.discard((upper_part_name, 'o', output_upper))
+                        nodes.discard((lower_part_name, 'r', output_upper))
+
+
+if __name__ == "main":
+    import donkeycar as dk
+    from os.path import expanduser
+
+    cfg = dk.load_config()
+    yml = expanduser('~/Python/donkeycar/donkeycar/templates/vehicle_recipes'
+                     '/test_vehicle.yml')
+    b = Builder(cfg, yml)
+    v = b.build_vehicle()
+    # v.start()
+
+    b.plot_vehicle(v)
 

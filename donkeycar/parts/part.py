5283c5bbcdf6a03480686bafd4db8c22a576cc55
DocGarbanzo <dirk.prange@web.de>
2022-04-05 22:37:05 +0100

    Fix registration issue.

--- donkeycar/parts/part.py
+++ donkeycar/parts/part.py
@@ -9,39 +9,39 @@ class CreatableFactory(type):
     """
     Metaclass to hold the registration dictionary of the part creation function
     """
-    register = {}
+    registry = {}
 
     def __init__(cls, name, bases, dct):
         super().__init__(name, bases, dct)
         l_name = name.lower()
         # don't register base class constructor
         if l_name != 'creatable':
-            cls.register[l_name] = cls
+            cls.registry[l_name] = cls
 
     @classmethod
     def make(mcs, concrete, cfg, kwargs):
-        return mcs.register[concrete.lower()].create(cfg, kwargs)
+        return mcs.registry[concrete.lower()].create(cfg, kwargs)
 
     @classmethod
     def get_all_classes(cls):
-        return list(cls.register.keys())
+        return list(cls.registry.keys())
 
     @classmethod
     def get_docstring_of_class(cls, creatable):
-        return cls.register[creatable].__doc__
+        return cls.registry[creatable].__doc__
 
     @classmethod
     def get_docstring_of_init_class(cls, creatable):
-        return cls.register[creatable].__init__.__doc__
+        return cls.registry[creatable].__init__.__doc__
 
     @classmethod
     def get_init_args_of_class(cls, creatable):
-        return list(cls.register[creatable].__init__.__code__.co_varnames)[1:]
+        return list(cls.registry[creatable].__init__.__code__.co_varnames)[1:]
 
     @classmethod
     def get_docstring_of_run(cls, creatable):
         try:
-            return cls.register[creatable].run.__doc__
+            return cls.registry[creatable].run.__doc__
         except AttributeError:
             logger.warning(f'Part {creatable} has no run method')
             return ''
@@ -49,7 +49,7 @@ class CreatableFactory(type):
     @classmethod
     def get_docstring_of_run_threaded(cls, creatable):
         try:
-            return cls.register[creatable].run_threaded.__doc__
+            return cls.registry[creatable].run_threaded.__doc__
         except AttributeError:
             logger.warning(f'Part {creatable} has no run_threaded method')
             return ''
@@ -57,7 +57,7 @@ class CreatableFactory(type):
     @classmethod
     def pretty_print_of_class(cls, creatable):
         s = '-' * 80 + '\n'
-        s += cls.register[creatable].__name__ + '\n'
+        s += cls.registry[creatable].__name__ + '\n'
         s += '-' * 80 + '\n'
         s += cls.get_description(cls.get_docstring_of_class(creatable)) + '\n'
         s += '-' * 80 + '\n'

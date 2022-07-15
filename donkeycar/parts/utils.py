e7787e74c9b14a049ca0a14a7e6c432168b12074
DocGarbanzo <dirk.prange@web.de>
2022-07-15 21:03:31 +0100

    Finished redesigned Dispatcher, but saved constructor arguments don't correctly parse lists.

--- donkeycar/parts/utils.py
+++ donkeycar/parts/utils.py
@@ -20,53 +20,20 @@ class Pipeline:
 
 class Dispatcher(Creatable):
     """
-    This part is a generic dispatcher that reads an index i and a tuple of
-    arguments and then returns the i'th argument. This logic is used in the
-    car app for example when we want to switch between user/throttle and
-    pilot/throttle depending on another user input, for example the web
-    controller button or a remote control button.
-    """
-    part_type = PartType.PROCESS
-
-    def __init__(self, num_args=2):
-        """
-        Creating part Dispatcher
-
-        :param int num_args: expected number of arguments, defaults to two.
-        """
-        super().__init__(num_args=num_args)
-        self.num_args = num_args
-
-    def run(self, index=0, *args):
-        """
-        Donkeycar parts interface
-
-        :param int index:   index of argument to return
-        :param tuple args:  tuple of arguments
-        :return:            chosen argument
-        """
-        assert len(args) == self.num_args, \
-            f"Expected {self.num_args} arguments, but got {len(args)}"
-        assert int(index) < self.num_args, \
-            f"Can only use index < {self.num_args}"
-        return args[int(index)]
-
-
-class MultiDispatcher(Creatable):
-    """
     This part is a generic dispatcher that is constructed with a list of
-    keys. In the vehicle loop it compares the first input to the elements of
-    the key list and finds its index n. Then it returns the n'th input of the
-    remaining arguments.
+    keys. In the vehicle loop's run method, it compares the first input to
+    the elements of the key list and finds its index n. Then it returns the
+    n'th input of the remaining arguments.
 
     For example:
 
-    m = MultiDispatcher(keys=['cat', 'dog', 'bird'])
+    m = Dispatcher(keys=['cat', 'dog', 'bird'])
     output = m.run('dog', input_0, input_1, input_2)
 
     will return input_1 as output, because 'dog' is the second element of the
-    key list, i.e. it has index 1. Note, the key needs to match one list
-    element, otherwise an error is thrown.
+    key list, i.e. it has index 1. As usual, array indexes start at zero.
+    Note, the key needs to match one list element, otherwise an error is
+    thrown.
 
     Alternatively the Dispatcher can be initialised without any keys and the
     index can be passed directly to the run function:
@@ -77,49 +44,55 @@ class MultiDispatcher(Creatable):
     will return input_2 as output. Note the index needs to be smaller than
     the number of following arguments.
 
-    This dispatcher logic is used in the car app for switching between
-    user/throttle and pilot/throttle depending on another user input,
+    This dispatcher logic is typically used in the car app for switching
+    between user/throttle and pilot/throttle depending on another user input,
     like the web controller mode or a remote control button.
     """
     part_type = PartType.PROCESS
 
     def __init__(self, keys=None):
         """
-        Creating part MultiDispatcher
+        Creating part Dispatcher
 
-        :param int num_args: expected number of arguments, defaults to two.
+        :param list keys:   List of expected keys to compare against.
+                            Optional parameter, if not provided, then the run
+                            method will require an integer as the first
+                            argument.
         """
+        if keys is not None:
+            assert isinstance(keys, list), f"Input keys can only be None or " \
+                                           f"list but it was {keys}"
+            assert len(list) > 1, \
+                f'Input list needs more than one entries, but has {len(list)}'
         super().__init__(keys=keys)
         self.keys = keys
 
     def run(self, key, *args):
         """
-        The method returns a single argument of the *args tuple that is
-        passed in. The selection depends on the key argument passed in first.
-        If the Dispatcher part has been initialised with a list, then the key
-        will be found in the list and the argument of the matching index is
-        returned. If key is an integer, the corresponding argument will be
-        returned. Note, counting starts at zero.
+        The method returns a single argument of the *args tuple that is passed
+        in. The selection depends on the key argument passed in first. If the
+        Dispatcher part has been initialised with a list, then the key will be
+        found in the list and the argument of the matching index is returned. If
+        key is an integer, the corresponding argument will be returned. Note,
+        counting starts at zero.
 
         :param int/any key:     Either index of argument to return, or key to
                                 compare against key list passed in construction.
         :param tuple args:      Tuple of arguments
-        :return:                Chosen argument
+        :return any:            Input argument at position of key
         """
-        # multiple asserts first
+        # if key is int or bool
         if isinstance(key, (int, bool)):
-            assert self.keys == None, \
+            assert self.keys is None, \
                 "Cannot call run() method with an integer or bool index, " \
                 "because Dispatcher has been created with a key list."
             # cast to int, in case it is a bool
             index = int(key)
             assert index < len(args)
-
-        assert len(args) == self.num_args, \
-            f"Expected {self.num_args} arguments, but got {len(args)}"
-        assert int(index) < self.num_args, \
-            f"Can only use index < {self.num_args}"
-        return args[int(index)]
+        # otherwise key must be in list (and is usually a string)
+        else:
+            index = self.keys.index(key)
+        return args[index]
 
 
 class Checker(Creatable):

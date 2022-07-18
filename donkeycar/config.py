3fa1605df389623bd3f042b7f52858093b0f1745
DocGarbanzo <dirk.prange@web.de>
2022-07-18 21:56:33 +0100

    Added support for own parts in the 'myparts' directory within the mycar directory. Now everyone can add parts which are registered in the factory and can be generated from there.
    Also cleaned up the config implementation a bit. Removed obsolete config parameters TARGET_H, etc and changed printing to logging.

--- donkeycar/config.py
+++ donkeycar/config.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 13 21:27:44 2017
-
-@author: wroscoe
-"""
+import glob
 import os
+import sys
 import types
-from logging import getLogger
+import logging
+import importlib.util
 
-logger = getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 class Config:
@@ -24,12 +21,12 @@ class Config:
             raise
         self.from_object(d)
         return True
-    
+
     def from_object(self, obj):
         for key in dir(obj):
             if key.isupper():
                 setattr(self, key, getattr(obj, key))
-                
+
     def __str__(self):
         result = []
         for key in dir(self):
@@ -53,19 +50,35 @@ def load_config(config_path=None, myconfig="myconfig.py"):
             local_config = os.path.join(os.path.curdir, 'config.py')
             if os.path.exists(local_config):
                 config_path = local_config
-    
-    logger.info(f'loading config file: {config_path}')
+
+    logger.info(f'Loading config file: {config_path}')
     cfg = Config()
     cfg.from_pyfile(config_path)
 
     # look for the optional myconfig.py in the same path.
     personal_cfg_path = config_path.replace("config.py", myconfig)
     if os.path.exists(personal_cfg_path):
-        logger.info(f"loading personal config over-rides from {myconfig}")
+        logger.info(f"Loading personal config over-rides from {myconfig}")
         personal_cfg = Config()
         personal_cfg.from_pyfile(personal_cfg_path)
         cfg.from_object(personal_cfg)
     else:
-        logger.warning(f"personal config: file not found {personal_cfg_path}")
-
+        logger.warning(f"Personal config file {myconfig} not found at"
+                       f" {personal_cfg_path}")
+    # check if myparts path is given and import modules from there
+    myparts_path = getattr(cfg, 'MYPARTS_PATH', None)
+    if myparts_path:
+        file_path = glob.glob(os.path.join(myparts_path, '*.py'))
+        for f in file_path:
+            try:
+                module_name = os.path.splitext(os.path.basename(f))[0]
+                spec = importlib.util.spec_from_file_location(module_name, f)
+                module = importlib.util.module_from_spec(spec)
+                assert module_name not in sys.modules, \
+                    f"Module {module_name} already loaded, skipping..."
+                sys.modules[module_name] = module
+                spec.loader.exec_module(module)
+                logger.info(f'Imported module {module_name}')
+            except Exception as e:
+                logger.error(e)
     return cfg

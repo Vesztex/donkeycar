153dbf58ca48cde2bdd1af2d7fc54ea0435df200
DocGarbanzo <dirk.prange@web.de>
2022-08-01 19:33:14 +0100

    Add missing pyyaml to setup.py

--- setup.py
+++ setup.py
@@ -50,7 +50,8 @@ setup(name='donkeycar',
           'typing_extensions',
           'pyfiglet',
           'psutil',
-          'docstring_parser'
+          'docstring_parser',
+          'pyyaml'
           "pynmea2",
           'pyserial',
           "utm",

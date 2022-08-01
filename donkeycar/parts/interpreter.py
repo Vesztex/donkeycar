763514999c4fa0a34a74dad072b92c6879ff3f22
DocGarbanzo <dirk.prange@web.de>
2022-08-01 19:42:24 +0100

    Move pytorch to local import

--- donkeycar/parts/interpreter.py
+++ donkeycar/parts/interpreter.py
@@ -5,7 +5,6 @@ import numpy as np
 from typing import Union, Sequence, List
 
 import tensorflow as tf
-import torch
 from tensorflow import keras
 
 from tensorflow.python.framework.convert_to_constants import \
@@ -205,7 +204,7 @@ class FastAIInterpreter(Interpreter):
 
     def predict(self, img_arr: np.ndarray, other_arr: np.ndarray) \
             -> Sequence[Union[float, np.ndarray]]:
-
+        import torch
         inputs = torch.unsqueeze(img_arr, 0)
         if other_arr is not None:
             #other_arr = np.expand_dims(other_arr, axis=0)
@@ -213,6 +212,7 @@ class FastAIInterpreter(Interpreter):
         return self.invoke(inputs)
 
     def load(self, model_path: str) -> None:
+        import torch
         logger.info(f'Loading model {model_path}')
         if torch.cuda.is_available():
             logger.info("using cuda for torch inference")

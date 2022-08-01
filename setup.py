b4dbc2ebc1f800015e7de79f9694b7956d326c1b
DocGarbanzo <dirk.prange@web.de>
2022-08-01 19:38:35 +0100

    Add missing graphviz to setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(name='donkeycar',
           'psutil',
           'docstring_parser',
           'pyyaml',
-          'python-graphviz'
+          'graphviz'
           "pynmea2",
           'pyserial',
           "utm",

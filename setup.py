613a0f7d37eb55ece322d33ca767b3eb240d3894
DocGarbanzo <dirk.prange@web.de>
2022-08-15 21:57:01 +0100

    bump version to 5.0.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='donkeycar',
-      version="4.3.22",
+      version="5.0.0",
       long_description=long_description,
       description='Self driving library for python.',
       url='https://github.com/autorope/donkeycar',

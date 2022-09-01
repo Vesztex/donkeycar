d90cb26c3cd43444b9592e1639c3f0ab755a1b61
DocGarbanzo <dirk.prange@web.de>
2022-09-01 22:08:59 +0100

    Gym template: - Improve DonkeyGymEnv to return vectors for vel, accel, gyro, pos/pos - Integrate into assembly

--- donkeycar/parts/dgym.py
+++ donkeycar/parts/dgym.py
@@ -13,9 +13,7 @@ class DonkeyGymEnv(Part):
 
     def __init__(self, sim_path, host="127.0.0.1", port=9091,
                  env_name="donkey-generated-track-v0",
-                 conf={}, record_location=False,
-                 record_gyroaccel=False, record_velocity=False,
-                 record_lidar=False, delay=0):
+                 conf={}, delay=0):
         """
         Creation of DonkeyGymEnv part.
 
@@ -24,18 +22,10 @@ class DonkeyGymEnv(Part):
         :param port:
         :param env_name:
         :param conf:
-        :param record_location:
-        :param record_gyroaccel:
-        :param record_velocity:
-        :param record_lidar:
         :param delay:
         """
         super().__init__(sim_path=sim_path, host=host, port=port,
-                         env_name=env_name, conf=conf,
-                         record_location=record_location,
-                         record_gyroaccel=record_gyroaccel,
-                         record_velocity=record_velocity,
-                         record_lidar=record_lidar)
+                         env_name=env_name, conf=conf)
 
         if sim_path != "remote":
             if not os.path.exists(sim_path):
@@ -61,10 +51,6 @@ class DonkeyGymEnv(Part):
                      'accel': (0., 0., 0.),
                      'vel': (0., 0., 0.)}
         self.delay = float(delay) / 1000
-        self.record_location = record_location
-        self.record_gyroaccel = record_gyroaccel
-        self.record_velocity = record_velocity
-        self.record_lidar = record_lidar
         self.buffer = []
 
     def delay_buffer(self, frame, info):
@@ -101,20 +87,17 @@ class DonkeyGymEnv(Part):
 
         self.action = [steering, throttle, brake]
 
-        # Output Sim-car position information if configured
-        outputs = [self.frame]
-        if self.record_location:
-            outputs += self.info['pos'][0],  self.info['pos'][1],  self.info['pos'][2],  self.info['speed'], self.info['cte']
-        if self.record_gyroaccel:
-            outputs += self.info['gyro'][0], self.info['gyro'][1], self.info['gyro'][2], self.info['accel'][0], self.info['accel'][1], self.info['accel'][2]
-        if self.record_velocity:
-            outputs += self.info['vel'][0],  self.info['vel'][1],  self.info['vel'][2]
-        if self.record_lidar:
-            outputs += self.info['lidar']
-        if len(outputs) == 1:
-            return self.frame
-        else:
-            return outputs
+        # Output Sim-car camera and position / imu / lidar information in a
+        # tuple. Missing values from the sim will be returned as None.
+        outputs = (self.frame,
+                   self.info.get('pos'),
+                   self.info.get('speed'),
+                   self.info.get('cte'),
+                   self.info.get('gyro'),
+                   self.info.get('accel'),
+                   self.info.get('vel'),
+                   self.info.get('lidar'))
+        return outputs
 
     def shutdown(self):
         self.running = False

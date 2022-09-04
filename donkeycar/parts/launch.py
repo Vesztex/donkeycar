727d600cc55559cf41d3298dcb3141c59f142995
DocGarbanzo <dirk.prange@web.de>
2022-09-04 20:51:33 +0100

    Gym template: - Adding support for AiLaunch part - Adding AiLaunch to gym assembly - Adding AiLaunch cfg parameters to cfg_basic.py

--- donkeycar/parts/launch.py
+++ donkeycar/parts/launch.py
@@ -1,13 +1,32 @@
 import time
+import logging
+from donkeycar.parts import Part
 
-class AiLaunch():
+logger = logging.getLogger(__name__)
+
+
+class AiLaunch(Part):
     '''
     This part will apply a large thrust on initial activation. This is to help
     in racing to start fast and then the ai will take over quickly when it's
     up to speed.
     '''
 
-    def __init__(self, launch_duration=1.0, launch_throttle=1.0, keep_enabled=False):
+    def __init__(self, launch_duration=1.0, launch_throttle=1.0,
+                 keep_enabled=False):
+        """
+        Creation of the AiLaunch part
+        :param float launch_duration:   How long to give launch throttle in
+                                        seconds (default 1.0)
+        :param float launch_throttle:   How much launch throttle (default 1.0)
+        :param bool keep_enabled:       Keeping it enabled after launch (
+                                        default False)
+        """
+        # always call Part constructor to make arguments available in
+        # documentation
+        super().__init__(launch_duration=launch_duration,
+                         launch_throttle=launch_throttle,
+                         keep_enabled=keep_enabled)
         self.active = False
         self.enabled = False
         self.timer_start = None
@@ -18,7 +37,7 @@ class AiLaunch():
         
     def enable_ai_launch(self):
         self.enabled = True
-        print('AiLauncher is enabled.')
+        logger.info('AiLauncher is enabled.')
 
     def run(self, mode, ai_throttle):
         new_throttle = ai_throttle
@@ -41,7 +60,7 @@ class AiLaunch():
             self.active = False
 
         if self.active:
-            print('AiLauncher is active!!!')
+            logger.info('AiLauncher is active!!!')
             new_throttle = self.launch_throttle
 
         return new_throttle

7f39b1c7122e19c1bb81a6159307d4679229a655
DocGarbanzo <dirk.prange@web.de>
2023-01-23 22:33:28 +0000

    Extend pipeline generator to cope with weights.

--- donkeycar/pipeline/sequence.py
+++ donkeycar/pipeline/sequence.py
@@ -1,20 +1,39 @@
-from typing import Callable, Iterator, List, Sized, Dict, Tuple
+from typing import Callable, Iterator, List, Sized, Dict, Tuple, Iterable
 from donkeycar.pipeline.types import TubRecord
 
 
 class PipelineGenerator(Sized):
     def __init__(self,
-                 records: List[TubRecord],
+                 records: Iterable[TubRecord],
                  x_transform: Callable[[TubRecord], Dict],
-                 y_transform: Callable[[TubRecord], Dict]) -> None:
+                 y_transform: Callable[[TubRecord], Dict],
+                 w_transform: Callable[[TubRecord], Dict] = None) -> None:
+        """
+        Generator for TF data. Reads an iterable of tub records and provides
+        a generator for iterating through the tub records and returning a
+        tuple of (x, y) or (x, y, z) where x, y are the models inputs and
+        outputs in dictionary form and z is an optional dictionary of
+        weights. For consistency z needs to have the same keys TensorShapes
+        as y.
+
+        :param records:         Iterable of TubRecords
+        :param x_transform:     Callable to extract x from TubRecord
+        :param y_transform:     Callable to extract y from TubRecord
+        :param w_transform:     Callable to extract weights from TubRecord
+        """
         self.records = records
         self.x_transform = x_transform
         self.y_transform = y_transform
+        self.w_transform = w_transform
 
     def __iter__(self) -> Iterator[Tuple[Dict, Dict]]:
         for record in self.records:
-            yield self.x_transform(record), self.y_transform(record)
+            ret = self.x_transform(record), self.y_transform(record)
+            if self.w_transform:
+                ret = *ret, self.w_transform(record)
+            yield ret
 
     def __len__(self) -> int:
+        # this might fail if self.records is not a list (i.e. Sizable).
         return len(self.records)
 
